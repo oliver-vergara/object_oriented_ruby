bd6962f416210bf2155a7ff53d4713227aa87a65
Oliver <deftool@outlook.com>
2016-04-04 20:42:51 -0700

    initial commit

---
+++ store_item.rb
@@ -0,0 +1,18 @@
+clothing = [
+  {type: "shirt",
+    color: "blue",
+    price: 20,
+    quantity: 100
+  },
+  {type: "jeans",
+    color: "denim",
+    price: 60,
+    quantity: 80
+  },
+  {type: "hoodie",
+    color: "grey",
+    price: 35,
+    quantity: 70}
+]
+
+p clothing
